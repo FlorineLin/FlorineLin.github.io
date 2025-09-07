8c9900989f1ba45ac0d203a4be7684206e2c868e
FlorineLin <2532675935@qq.com>
2025-09-08 01:05:10 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'learning-achievements-and-skills', 'CampusActivities']
+const section_names = ['home', 'learning-achievements-and-skills', 'CampusActivities', 'Internship-and-Practices']
 
 
 window.addEventListener('DOMContentLoaded', event => {
